be2c73a6e183deafe806de0c62f5c4b752a12812
amantha1 <amanthag1@gmail.com>
2024-01-06 13:43:32 +0530

    Jenkinsfile

---
+++ src/main/java/com/pack/pages/LandingPage.java
@@ -0,0 +1,67 @@
+package com.pack.pages;
+
+import com.pack.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LandingPage extends TestBase {
+
+    @FindBy(xpath = "//img[@src='https://human-backend-data.s3.eu-west-2.amazonaws.com/6322eced057b8bea7c748e6f/organization/1665732870421.jpg']")
+    WebElement Logo;
+
+    @FindBy(xpath = "//h3[contains(.,'Thank Staff')]")
+    WebElement ThankStaffLabel;
+
+    @FindBy(xpath = "//p[contains(.,'Sending a message or gift to the staff you appreciate is just a few clicks away!')]")
+    WebElement Text;
+
+    @FindBy(xpath = "//button[@class='btn w-100 primary'][contains(.,'Continue')]")
+    WebElement ContinueBtn;
+
+    @FindBy(xpath = "//a[@href='https://humansofsharegratitude.com/terms-of-use/'][contains(.,'Terms of Service')]")
+    WebElement TermsofService;
+
+    @FindBy(xpath = "//a[@href='https://humansofsharegratitude.com/acceptable-use-policy/'][contains(.,'Acceptable Use Policy')]")
+    WebElement AcceptableUsePolicy;
+
+    @FindBy(xpath = "//a[@href='https://humansofsharegratitude.com/privacy-policy-2/'][contains(.,'Privacy Policy')]")
+    WebElement PrivacyPolicy;
+
+
+    public LandingPage(){
+        PageFactory.initElements(driver, this);
+    }
+
+    public String validateLandingPageTitle(){
+        return driver.getTitle();
+    }
+
+    public boolean verifyLogo(){
+        return Logo.isDisplayed();
+    }
+
+    public boolean verifyThankStaffLabel(){
+        return ThankStaffLabel.isDisplayed();
+    }
+
+    public boolean verifyText(){
+        return Text.isDisplayed();
+    }
+
+    public boolean verifyContinueBtn(){
+        return ContinueBtn.isDisplayed();
+    }
+
+    public boolean verifyTermsofService(){
+        return TermsofService.isDisplayed();
+    }
+
+    public boolean verifyAcceptableUsePolicy(){
+        return AcceptableUsePolicy.isDisplayed();
+    }
+
+    public boolean verifyPrivacyPolicy(){
+        return PrivacyPolicy.isDisplayed();
+    }
+}
---
+++ src/test/java/com/pack/testcases/LandingPageTest.java
@@ -0,0 +1,50 @@
+package com.pack.testcases;
+
+import com.pack.base.TestBase;
+import com.pack.pages.LandingPage;
+import com.pack.util.TestUtil;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LandingPageTest extends TestBase {
+
+    LandingPage landingPage;
+    TestUtil testUtil;
+
+
+    public LandingPageTest(){
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        testUtil = new TestUtil();
+        landingPage = new LandingPage();
+    }
+
+
+    @Test(priority=1)
+    public void verifyLandingPageTitleTest(){
+        String landingPageTitle = landingPage.validateLandingPageTitle();
+        Assert.assertEquals(landingPageTitle, "ShareGratitude");
+    }
+
+    @Test(priority=2)
+    public void verifyLandingPageTest(){
+        landingPage.verifyLogo();
+        landingPage.verifyThankStaffLabel();
+        landingPage.verifyText();
+        landingPage.verifyContinueBtn();
+        landingPage.verifyTermsofService();
+        landingPage.verifyAcceptableUsePolicy();
+        landingPage.verifyPrivacyPolicy();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
